bd6246d1535621bea88121a3dabef03792303168
ilanah <ilana.hakim@gmail.com>
2017-06-20 20:48:39 +0300

    move firebase_config to external file that will not be commited

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,7 @@ import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 import firebase from 'firebase';
-
-const firebase_config = {
-  apiKey: "AIzaSyBQDs25w1rBKyamg6DcWBy7yQv6f4G-uc0",
-  authDomain: "love-spread.firebaseapp.com",
-  databaseURL: "https://love-spread.firebaseio.com",
-  projectId: "love-spread",
-  storageBucket: "love-spread.appspot.com",
-  messagingSenderId: "522191357045"
-};
+import firebase_config from './firebase_config';
 
 firebase.initializeApp(firebase_config);
 
