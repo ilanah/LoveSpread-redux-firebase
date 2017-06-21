02d9fe4efcad26580761c9a7a51d7ec288612dc8
ilanah <ilana.hakim@gmail.com>
2017-06-21 10:52:57 +0300

    example for firebase_config file
    rename to firebase_config.js and set your own keys

---
+++ src/store/_firebase_config.js
@@ -0,0 +1,9 @@
+// TODO: Replace with your project's customized code snippet
+const firebase_config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+};
+export default firebase_config;
